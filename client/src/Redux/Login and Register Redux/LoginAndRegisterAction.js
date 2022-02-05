eb2ef69dbce8fded998c90ea433df335d3ec243e
Dholakia Vaibhav <vdholakia04@gmail.com>
2022-02-05 14:04:47 +0530

    refs #main, redux folder fix

---
+++ client/src/Redux/Login and Register Redux/LoginAndRegisterAction.js
@@ -0,0 +1,103 @@
+import {
+  toggleUserIsToRegister,
+  formUserData,
+  setAuthLocalStorageAfterAccess,
+  userLogout,
+  formHasError,
+  formErrorData,
+  loadingForButton,
+} from './LoginAndRegisterSlice';
+import * as api from '../../API/apis.js';
+
+// Initial user data when page loads or reopens
+const initialFormData = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+// COMMON ACTIONS
+export const toggleUserIsToRegisterAction = (request) => async (dispatch) => {
+  dispatch(toggleUserIsToRegister(request));
+};
+
+export const formUserDataAction = (request) => async (dispatch) => {
+  dispatch(formUserData(request));
+};
+
+export const formHasErrorAction = (request) => async (dispatch) => {
+  dispatch(formHasError(request));
+};
+
+export const initialFormDataAction = () => async (dispatch) => {
+  dispatch(formUserData(initialFormData));
+};
+
+export const loadingForButtonAction = (request) => async (dispatch) => {
+  dispatch(loadingForButton(request));
+};
+
+// API ACTIONS
+
+// LOGIN ACTION
+export const loginAction = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.login(formData);
+    dispatch(setAuthLocalStorageAfterAccess(data));
+    navigate('/');
+  } catch (error) {
+    dispatch(formHasError(true));
+    dispatch(loadingForButton(false));
+    dispatch(
+      formErrorData(
+        error && error.response && error.response.data
+          ? error.response.data
+          : { message: 'Unexpected response. Please try again.' }
+      )
+    );
+  }
+};
+
+// REGISTER ACTION
+export const registerAction = (formData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.register(formData);
+    dispatch(setAuthLocalStorageAfterAccess(data));
+    navigate('/');
+  } catch (error) {
+    dispatch(formHasError(true));
+    dispatch(loadingForButton(false));
+    dispatch(
+      formErrorData(
+        error && error.response && error.response.data
+          ? error.response.data
+          : { message: 'Unexpected response. Please try again.' }
+      )
+    );
+  }
+};
+
+// GOOGLE LOGIN ACTION
+export const googleLoginAction = (userData, navigate) => async (dispatch) => {
+  try {
+    const { data } = await api.googleLogin(userData.profileObj);
+    dispatch(setAuthLocalStorageAfterAccess(data));
+    navigate('/');
+  } catch (error) {
+    dispatch(formHasError(true));
+    dispatch(
+      formErrorData(
+        error && error.response && error.response.data
+          ? error.response.data
+          : { message: 'Unexpected response. Please try again.' }
+      )
+    );
+  }
+};
+
+// LOGOUT ACTION
+export const logoutAction = () => async (dispatch) => {
+  dispatch(userLogout());
+};
