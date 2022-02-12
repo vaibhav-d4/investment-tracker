ec199df8e6c340d118619de28dff84dda9bc5b12
Dholakia Vaibhav <vdholakia04@gmail.com>
2022-02-12 21:21:25 +0530

    refs #STOCKS-Feature, Added add transaction dialog

--- client/src/Common/Utils/Styles/DialogStyles.js
+++ client/src/Common/Utils/Styles/DialogStyles.js
@@ -1,3 +1,7 @@
 import { makeStyles } from '@mui/styles';
 
-export default makeStyles((theme) => ({}));
+export default makeStyles((theme) => ({
+  inputFields: {
+    marginTop: theme.spacing(1),
+  },
+}));
--- client/src/Components/Stocks/Stocks DataGrid/Add Transaction Dialog/AddTransactionDialog.js
+++ client/src/Components/Stocks/Stocks DataGrid/Add Transaction Dialog/AddTransactionDialog.js
@@ -3,16 +3,24 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 // MUI IMPORTS
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, Grid, Box, TextField } from '@mui/material';
+import {
+  LoadingButton,
+  // AdapterDateFns, LocalizationProvider, DatePicker
+} from '@mui/lab';
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 
 // COMPONENTS IMPORTS
+import useDialogStyles from '../../../../Common/Utils/Styles/DialogStyles';
 import InputFieldComponent from '../../../../Common/Utils/Component Utils/InputFieldComponent';
 
 // REDUX ACTIONS IMPORTS
 import { isAddTransactionDialogOpenAction } from '../../../../Redux/Stocks Redux/StocksActions';
 
 const AddTransactionDialog = () => {
+  const dialogClasses = useDialogStyles();
   const dispatch = useDispatch();
 
   const isAddTransactionDialogOpen = useSelector((state) => state.stocks.isAddTransactionDialogOpen);
@@ -21,19 +29,77 @@ const AddTransactionDialog = () => {
     dispatch(isAddTransactionDialogOpenAction(false));
   };
 
-  const handleInputDataChange = () => {};
+  const handleInputDataChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleDateChange = (newDateValue) => {
+    console.log(newDateValue);
+  };
+
   return (
     <>
       <Dialog open={isAddTransactionDialogOpen} onClose={handleDialogClose} scroll='paper' fullWidth maxWidth='sm'>
         <DialogTitle>Add a New Transaction</DialogTitle>
         <DialogContent>
-          <InputFieldComponent
-            name='depositoryName'
-            label='Depository Name'
-            handleChange={handleInputDataChange}
-            required
-            fullWidth
-          />
+          <Box component='form' autoComplete='off' className={dialogClasses.inputFields}>
+            <Grid container spacing={2}>
+              <InputFieldComponent
+                name='depositoryName'
+                label='Depository Name'
+                handleChange={handleInputDataChange}
+                required
+                fullWidth
+              />
+              <InputFieldComponent
+                name='companyName'
+                label='Company Name'
+                handleChange={handleInputDataChange}
+                required
+                fullWidth
+              />
+              <InputFieldComponent
+                name='googleSymbol'
+                label='Google Symbol'
+                handleChange={handleInputDataChange}
+                required
+                half
+              />
+              <InputFieldComponent
+                name='yahooSymbol'
+                label='Yahoo Symbol'
+                handleChange={handleInputDataChange}
+                required
+                half
+              />
+              <Grid item xs={12} sm={3} /> {/* EMPTY GRID ITEM */}
+              <Grid item xs={12} sm={6}>
+                <LocalizationProvider dateAdapter={DateAdapter}>
+                  <DatePicker
+                    label='Buy Date'
+                    // value={}
+                    onChange={handleDateChange}
+                    renderInput={(params) => <TextField required fullWidth {...params} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} sm={3} /> {/* EMPTY GRID ITEM */}
+              <InputFieldComponent
+                name='noOfShares'
+                label='Number of Shares'
+                handleChange={handleInputDataChange}
+                required
+                half
+              />
+              <InputFieldComponent
+                name='priceOfShareAtBuy'
+                label='Price of Share when Bought'
+                handleChange={handleInputDataChange}
+                required
+                half
+              />
+            </Grid>
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button variant='text' color='primary' onClick={handleDialogClose}>
